080e183b30f9eba095654784860d54f8fa3cb08a
pietre.linux <pietre.linux@gmail.com>
2019-01-29 19:20:41 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,8 @@
-#!/bin/bash 
+#!/bin/sh
+
+sudo apt-get install mysql-server apache2 libapache2-mod-php7.0 php7.0 php7.0-xml php7.0-gd php7.0-json php7.0-zip php7.0-intl php7.0-mcrypt php7.0-curl php7.0-intl php7.0-opcache mariadb-server php7.0-mysql
+
+a2enmod rewrite
+service apache2 restart
+a2enmod ssl
+service apache2 restart
