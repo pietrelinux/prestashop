f1f499403fbbd006678c828519b6e7a5ba24bbd7
pietre.linux <pietre.linux@gmail.com>
2020-03-28 21:11:42 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt-get install mysql-server apache2 libapache2-mod-php7.0 php7.0 php7.0-xml php7.0-gd php7.0-json php7.0-zip php7.0-intl php7.0-mcrypt php7.0-curl php7.0-intl php7.0-opcache mariadb-server php7.0-mysql
+sudo apt-get install zip unzip mysql-server apache2 libapache2-mod-php7.0 php7.0 php7.0-xml php7.0-gd php7.0-json php7.0-zip php7.0-intl php7.0-mcrypt php7.0-curl php7.0-intl php7.0-opcache mariadb-server php7.0-mysql
 
 a2enmod rewrite
 service apache2 restart
